a503919abbd86180658bdfee3678336789df7987
ticheler <ticheler@ff403467-3f20-0410-99e5-ff581e626a08>
2008-03-04 16:05:42 +0000

    MySQL longtext fix to avoid truncated metadata records
    git-svn-id: https://geonetwork.svn.sourceforge.net/svnroot/geonetwork/trunk@1184 ff403467-3f20-0410-99e5-ff581e626a08

--- gast/setup/sql/create-db-mysql.sql
+++ gast/setup/sql/create-db-mysql.sql
@@ -225,7 +225,7 @@ CREATE TABLE Metadata
     isHarvested  char(1)        default 'n' not null,
     createDate   varchar(24)    not null,
     changeDate   varchar(24)    not null,
-    data         text           not null,
+    data         longtext       not null,
     source       varchar(250)   not null,
     title        varchar(255),
     root         varchar(255),
