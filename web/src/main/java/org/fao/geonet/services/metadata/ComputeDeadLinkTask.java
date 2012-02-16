9376ad291d4dd0ec381b9c5b44e4b46c597a9492
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-02-16 08:59:29 +0100

    make checking deadlinks release database before testing all links so that it does not hold up other processes as long.
    also add timeouts

--- web/src/main/java/org/fao/geonet/services/metadata/ComputeDeadLinkTask.java
+++ web/src/main/java/org/fao/geonet/services/metadata/ComputeDeadLinkTask.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -42,7 +44,6 @@ public class ComputeDeadLinkTask extends TimerTask {
     public void run() {
    
         try {
-            Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
 
             FileWriter fstream = new FileWriter(outputXmlPath + "failingUrls-temp.xml");
             try {
@@ -56,6 +57,10 @@ public class ComputeDeadLinkTask extends TimerTask {
 
                 patt = Pattern.compile(regex);
 
+                // note, we are adding strings as url because performing hashcode on a url requires dns resolution and we don't want to do that
+                // until after database connection is closed.
+                HashMap<String /* URL */, String/* ID */> urls = new HashMap<String, String>();
+
                 String query = "SELECT DISTINCT                " +
                                "                 id            " +
                                "FROM                           " +
@@ -63,7 +68,10 @@ public class ComputeDeadLinkTask extends TimerTask {
                                "WHERE                          " +
                                "                 istemplate = 'n'";
                 
+                Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
+                try {
                 Element result = dbms.select(query);
+                
 
                 for (Iterator iter = result.getChildren().iterator(); iter.hasNext();) {
                     Element rec = (Element) iter.next();
@@ -77,14 +85,27 @@ public class ComputeDeadLinkTask extends TimerTask {
                     curMd = Xml.transform(curMd, context.getAppPath() + "xsl" + File.separator + "alltext.xsl");
                     String texterizedMd = curMd.getText();
 
-                    testTransferOptionURLs(fstream, patt, id, texterizedMd);
+                    testTransferOptionURLs(fstream, patt, id, texterizedMd, urls);
                 } // for ()
-
-                // -- explicitly close Dbms resource to avoid exhausting Dbms
+                } finally {
+                    // close resource manager so process of checking links
+                    // doesn't keep metadata table locked.
+                    // this can hang other processes
+                    // -- explicitly close Dbms resource to avoid exhausting
+                    // Dbms
+                    context.getResourceManager().close();
+                }
+                
+                // now that database is free we can take our time to check deadlinks
+                for(Map.Entry<String, String> entry: urls.entrySet()) {
+                    String id = entry.getValue();
+                    String urlText = entry.getKey();
+                    testURL(fstream, id, urlText);
+                }
+                
                 // pool
                 fstream.write("</response>");
             } finally {
-                context.getResourceManager().close();
                 IOUtils.closeQuietly(fstream);
             }
 
@@ -96,33 +117,40 @@ public class ComputeDeadLinkTask extends TimerTask {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
     }
 
-    private void testTransferOptionURLs(FileWriter fstream, Pattern patt, String id, String texterizedMd) throws IOException {
+    private void testTransferOptionURLs(FileWriter fstream, 
+            Pattern patt, String id, String texterizedMd, HashMap<String, String> urls) throws IOException {
         Matcher matcher = patt.matcher(texterizedMd);
 
         while (matcher.find()) {
             String text = matcher.group(0);
-            try {
-                URL testUrl = new URL(text);
-                try {
-                    HttpURLConnection c = (HttpURLConnection) testUrl.openConnection();
-                    try {
-                        if (c.getResponseCode() >= 400) {
-                            fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + text + "]]></badUrl>\n");
-                        }
-                    } finally {
-                        c.disconnect();
-                    }
-                } catch (IOException e) {
-                    fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + text + "]]></badUrl>\n");
-                }
-            } catch (MalformedURLException e) {
-
-                fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + text + "]]></badUrl>\n");
-            }
+            urls.put(text, id);
         } // while()
+    }
+
+    private void testURL(FileWriter fstream, String id, String urlText) throws MalformedURLException, IOException {
+        try {
+            URL testUrl = new URL(urlText);
+            try {
+                HttpURLConnection c = (HttpURLConnection) testUrl.openConnection();
+                c.setConnectTimeout(10000);
+                c.setReadTimeout(10000);
+                try {
+                    if (c.getResponseCode() >= 400) {
+                        fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + testUrl + "]]></badUrl>\n");
+                    }
+                } finally {
+                    c.disconnect();
+                }
+            } catch (IOException e) {
+                fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + testUrl + "]]></badUrl>\n");
+            }
+        } catch (MalformedURLException e) {
+
+            fstream.write("\t<badUrl mdId=\"" + id + "\"><![CDATA[" + urlText + "]]></badUrl>\n");
+        }
+
     }
 
     private void checkParentChildValidity(Dbms dbms, FileWriter fstream, GeonetContext gc, String id, Element curMd) {
