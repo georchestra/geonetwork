c9dbd325a9dc70d4ed025cc08b0fcaa588d43a79
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-02-29 11:31:39 +0100

    fixed debug of main page

--- web/src/main/java/org/fao/geonet/kernel/mef/IVisitor.java
+++ web/src/main/java/org/fao/geonet/kernel/mef/IVisitor.java
@@ -26,8 +26,6 @@ package org.fao.geonet.kernel.mef;
 import org.jdom.Element;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Register XML or binary file to process.
@@ -57,47 +55,5 @@ public interface IVisitor
 
 }
 
-// =============================================================================
-
-class InputStreamBridge extends InputStream
-{
-	// --------------------------------------------------------------------------
-	// ---
-	// --- Constructor
-	// ---
-	// --------------------------------------------------------------------------
-
-	public InputStreamBridge(InputStream is)
-	{
-		this.is = is;
-	}
-
-	// --------------------------------------------------------------------------
-	// ---
-	// --- Bridging methods
-	// ---
-	// --------------------------------------------------------------------------
-
-	public int read() throws IOException { return is.read(); }
-
-	public int available() throws IOException { return is.available(); }
-
-	// --- this *must* be empty to work with zip files
-	public void close() throws IOException {}
-
-	public synchronized void mark(int readlimit) { is.mark(readlimit); }
-
-	public synchronized void reset() throws IOException { is.reset(); }
-
-	public boolean markSupported() {	return is.markSupported(); }
-
-	// --------------------------------------------------------------------------
-	// ---
-	// --- Variables
-	// ---
-	// --------------------------------------------------------------------------
-
-	private InputStream is; 
-}
 // =============================================================================
 
