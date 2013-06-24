66eeeff1bb70cde965a1ede2075b04a8893a0722
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-06-24 15:40:10 +0200

    1211 sql migrate script: remove fields already present in db

--- web/src/main/webapp/WEB-INF/classes/setup/sql-georchestra/migrate/1211/db-migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-georchestra/migrate/1211/db-migrate-default.sql
@@ -192,7 +192,7 @@ CREATE TABLE Thesaurus (
 
 ALTER TABLE public.thesaurus OWNER TO "www-data";
 
-ALTER TABLE Languages ADD isocode varchar(3);
+--ALTER TABLE Languages ADD isocode varchar(3);
 ALTER TABLE Languages ADD isInspire char(1);
 ALTER TABLE Languages ADD isDefault char(1);
 
@@ -234,10 +234,10 @@ INSERT INTO Settings VALUES (912,910,'enableIsoView','true');
 INSERT INTO Settings VALUES (913,910,'enableInspireView','false');
 INSERT INTO Settings VALUES (914,910,'enableXmlView','true');
 INSERT INTO Settings VALUES (915,910,'defaultView','simple');
-INSERT INTO Settings VALUES (917,1,'metadataprivs',NULL);
-INSERT INTO Settings VALUES (918,917,'usergrouponly','false');
-INSERT INTO Settings VALUES (920,1,'threadedindexing',NULL);
-INSERT INTO Settings VALUES (921,920,'maxthreads','1');
+--INSERT INTO Settings VALUES (917,1,'metadataprivs',NULL);
+--INSERT INTO Settings VALUES (918,917,'usergrouponly','false');
+--INSERT INTO Settings VALUES (920,1,'threadedindexing',NULL);
+--INSERT INTO Settings VALUES (921,920,'maxthreads','1');
 INSERT INTO Settings VALUES (17,10,'svnUuid','');
 INSERT INTO Settings VALUES (180,173,'organizationName',NULL);
 INSERT INTO Settings VALUES (181,173,'postalAddress',NULL);
