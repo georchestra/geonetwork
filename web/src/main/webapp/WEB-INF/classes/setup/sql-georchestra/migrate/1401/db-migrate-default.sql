b9669b9cb40e88b23359902b1e65975ad0510898
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2014-01-30 16:04:52 +0100

    new release is 14.01, not 13.12

--- web/src/main/webapp/WEB-INF/classes/setup/sql-georchestra/migrate/1401/db-migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-georchestra/migrate/1401/db-migrate-default.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- Version update
-UPDATE Settings SET value='13.12' WHERE name='version';
+UPDATE Settings SET value='14.01' WHERE name='version';
 UPDATE Settings SET value='0' WHERE name='subVersion';
 
 COMMIT;
