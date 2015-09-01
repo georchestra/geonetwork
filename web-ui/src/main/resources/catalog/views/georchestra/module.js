1cfd99d0fbbe8dc47fd0f3510b8413e936be138f
Florent gravin <florent.gravin@camptocamp.com>
2015-09-01 12:54:43 +0200

    ui: add georchestra custom view this view just point on default view templates/css and just extend main scope and add specific services/directives

---
+++ web-ui/src/main/resources/catalog/views/georchestra/module.js
@@ -0,0 +1,58 @@
+(function() {
+
+  goog.provide('gn_search_georchestra');
+  goog.require('gn_search_default');
+  goog.require('georchestra_linkactions');
+
+  var module = angular.module('gn_search_georchestra',
+      ['gn_search_default', 'georchestra_linkactions']);
+
+  /**
+   * Overload default settings
+   */
+  module.run(['gnSearchSettings', function(gnSearchSettings) {
+
+    gnSearchSettings.linkTypes = {
+      links: ['LINK'],
+      downloads: ['DOWNLOAD'],
+      layers:['OGC', 'vnd.ogc.wms_xml'],
+      maps: ['ows']
+    };
+  }]);
+
+
+  /**
+   * ExtendMainController is a constructor of a new controller that is here
+   * to extend the `gnsDefault` controller, containing main logic of the
+   * default search page.
+   *
+   * @param {Object} $scope is shared with gnsDefault scope
+   * @param {Object} georLinkActionsService
+   * @constructor
+   */
+  var ExtendMainController = function($scope, georLinkActionsService) {
+    if($scope.resultviewFns) {
+
+      $scope.resultviewFns.addMdLayerToMap = georLinkActionsService.addWMSLayer;
+      $scope.resultviewFns.addAllMdLayersToMap = function(layers, md) {
+        georLinkActionsService.extractMetadata('mapfishapp', md.getId());
+      }
+    }
+  };
+
+  module.directive('gnExtendMainctrl', [
+    function() {
+      return {
+        restrict: 'E',
+        scope: false,
+        controller: ExtendMainController
+      };
+    }
+  ]);
+
+  ExtendMainController['$inject'] = [
+    '$scope',
+    'georLinkActionsService'
+  ];
+
+})();
