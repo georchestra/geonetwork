9c47a6a39393b54336b2c8204616cd495bae2b50
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2018-01-29 17:33:34 +0100

    GN3.8.2 - geOrchestra integration - fixing logging configuration
    (Cherry-picked from 9daab76460bc371d981ccb9375a385ac8c6ea432)

--- services/src/main/java/org/fao/geonet/api/site/LoggingApi.java
+++ services/src/main/java/org/fao/geonet/api/site/LoggingApi.java
@@ -23,29 +23,13 @@
 
 package org.fao.geonet.api.site;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.apache.commons.io.IOUtils;
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Logger;
-import org.fao.geonet.api.API;
-import org.fao.geonet.constants.Geonet;
-import org.fao.geonet.kernel.GeonetworkDataDirectory;
-import org.fao.geonet.api.site.model.ListLogFilesResponse;
-import org.fao.geonet.util.FileUtil;
-import org.fao.geonet.utils.Log;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_OPS;
+import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_TAG;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -56,8 +40,33 @@ import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_OPS;
-import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_TAG;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.fao.geonet.api.API;
+import org.fao.geonet.api.site.model.ListLogFilesResponse;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.kernel.GeonetworkDataDirectory;
+import org.fao.geonet.util.FileUtil;
+import org.fao.geonet.utils.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 
 @RequestMapping(value = {
@@ -71,6 +80,7 @@ import static org.fao.geonet.api.ApiParams.API_CLASS_CATALOG_TAG;
 @Controller("logging")
 @PreAuthorize("hasRole('Administrator')")
 public class LoggingApi {
+
     private final String regexp = "log4j(-(.*?))?\\.xml";
 
     @Autowired
@@ -92,6 +102,12 @@ public class LoggingApi {
         java.util.List<ListLogFilesResponse.LogFileResponse> logFileList =
             new ArrayList<>();
         String classesFolder = dataDirectory.getWebappDir() + "/WEB-INF/classes";
+        try {
+            Object logov = ApplicationContextHolder.get().getBean("logPlaceHolder");
+            classesFolder = logov.toString();
+        } catch (Throwable e) {
+          // default behaviour
+        }
         File folder = new File(classesFolder);
 
         if (folder != null && folder.isDirectory()) {
