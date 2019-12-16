ce74063a4e899e871d77c1d943f8dd2dcb9f7e40
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2019-12-16 18:54:10 +0100

    geor-utils - exporting configuration of the SchematronJob into the datadir
    See https://github.com/georchestra/datadir/commit/fed1199d4f2d299dbb0b1b8deaf2f05b5e37b62b
    and https://github.com/georchestra/geonetwork/issues/90

--- georchestra-utils/src/main/java/org/georchestra/geonetwork/SchematronJob.java
+++ georchestra-utils/src/main/java/org/georchestra/geonetwork/SchematronJob.java
@@ -1,24 +1,24 @@
 package org.georchestra.geonetwork;
 
-import static org.fao.geonet.repository.specification.MetadataSpecs.isHarvested;
-
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.log4j.Logger;
 import org.fao.geonet.ApplicationContextHolder;
 import org.fao.geonet.domain.Metadata;
+import org.fao.geonet.domain.MetadataType;
 import org.fao.geonet.kernel.datamanager.IMetadataValidator;
 import org.fao.geonet.repository.MetadataRepository;
+import org.fao.geonet.repository.specification.MetadataSpecs;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-
-import jeeves.server.context.ServiceContext;
 
 @Component
 public class SchematronJob extends QuartzJobBean {
@@ -30,9 +30,15 @@ public class SchematronJob extends QuartzJobBean {
     private ConfigurableApplicationContext applicationContext;
     @Autowired
     private IMetadataValidator metadataValidator;
+    @Value("${schematron.job.activated:false}")
+    private boolean activated;
 
     @Override
     protected void executeInternal(final JobExecutionContext jobExecContext) throws JobExecutionException {
+        if (activated == false) {
+            Log.info("schematronJob is not activated, skipping execution");
+            return;
+        }
         if (started.get() == true) {
             Log.info("Job already running, skipping execution");
             return;
@@ -46,14 +52,16 @@ public class SchematronJob extends QuartzJobBean {
             }
             ApplicationContextHolder.set(applicationContext);
             MetadataRepository mdrepo = applicationContext.getBean(MetadataRepository.class);
-            ServiceContext serviceContext = ServiceContext.get();
             
-            List<Integer> mdToValidate = mdrepo.findAllIdsBy((Specification<Metadata>) isHarvested(false));
+            final Specifications<Metadata> spec =
+                    Specifications.where((Specification<Metadata>) MetadataSpecs.isType(MetadataType.METADATA))
+                        .and((Specification<Metadata>) MetadataSpecs.isHarvested(false));
+
+            List<Integer> mdToValidate = mdrepo.findAllIdsBy(spec);
             for (Integer mdId : mdToValidate) {
                 Metadata record = mdrepo.findOne(mdId);
                 try {
-                	metadataValidator.validateMetadata(record.getDataInfo().getSchemaId(),
-                			record.getXmlData(false), serviceContext, " ");
+                    metadataValidator.doValidate(record, "eng");
                 } catch (Exception e) {
                     Log.error("Error validating metadata id " + record.getUuid());
                 }
