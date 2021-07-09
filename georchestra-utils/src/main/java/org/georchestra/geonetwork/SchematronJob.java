cdc2f05a01c5626a6cd84c14dfc2014b41fd7ede
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2021-07-09 16:56:31 +0200

    georchestra-utils - reintroducing the maven module
    the schematron job for validating the MDs regularly is not used by
    everyone in the community, but this will provide a placeholder for
    having custom code more easily anyway.

---
+++ georchestra-utils/src/main/java/org/georchestra/geonetwork/SchematronJob.java
@@ -0,0 +1,75 @@
+package org.georchestra.geonetwork;
+
+import org.apache.log4j.Logger;
+import org.fao.geonet.ApplicationContextHolder;
+import org.fao.geonet.domain.Metadata;
+import org.fao.geonet.domain.MetadataType;
+import org.fao.geonet.kernel.datamanager.IMetadataValidator;
+import org.fao.geonet.repository.MetadataRepository;
+import org.fao.geonet.repository.specification.MetadataSpecs;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component
+public class SchematronJob extends QuartzJobBean {
+
+    private AtomicBoolean started = new AtomicBoolean(false);
+    private final Logger Log = Logger.getLogger(this.getClass());
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+    @Autowired
+    private IMetadataValidator metadataValidator;
+    @Value("${schematron.job.activated:false}")
+    private boolean activated;
+
+    @Override
+    protected void executeInternal(final JobExecutionContext jobExecContext) throws JobExecutionException {
+        if (activated == false) {
+            Log.info("schematronJob is not activated, skipping execution");
+            return;
+        }
+        if (started.get() == true) {
+            Log.info("Job already running, skipping execution");
+            return;
+        }
+        try {
+            started.set(true);
+
+            if ((applicationContext == null) || (metadataValidator == null)) {
+                Log.error("applicationContext or _dataManager is null, skipping execution");
+                return;
+            }
+            ApplicationContextHolder.set(applicationContext);
+            MetadataRepository mdrepo = applicationContext.getBean(MetadataRepository.class);
+
+            final Specification<Metadata> spec =
+                    Specification.where((Specification<Metadata>) MetadataSpecs.isType(MetadataType.METADATA))
+                        .and((Specification<Metadata>) MetadataSpecs.isHarvested(false));
+
+            List<Integer> mdToValidate = mdrepo.findIdsBy(spec);
+            for (Integer mdId : mdToValidate) {
+                Metadata record = mdrepo.findOneById(mdId);
+                try {
+                    metadataValidator.doValidate(record, "eng");
+                } catch (Exception e) {
+                    Log.error("Error validating metadata id " + record.getUuid());
+                }
+            }
+        } catch (Exception e) {
+            Log.error("Exception occured: ", e);
+        } finally {
+            Log.info("Finished validation job");
+            started.set(false);
+        }
+    }
+}
