58a9df735882ab3765e7556c8640e4fd2f982ab4
Florent gravin <florent.gravin@camptocamp.com>
2021-09-13 15:35:15 +0200

    geor: open WMS links in Mapstore

--- web-ui/src/main/resources/catalog/views/georchestra/module.js
+++ web-ui/src/main/resources/catalog/views/georchestra/module.js
@@ -1,15 +1,13 @@
-(function() {
+(function () {
 
   goog.provide('gn_search_georchestra');
   goog.require('gn_search_default');
-  goog.require('georchestra_linkactions');
 
   var module = angular.module('gn_search_georchestra',
-    ['gn_search_default', 'georchestra_linkactions']);
-
+    ['gn_search_default']);
 
   // Add a custom georchestra locale file
-  module.config(['$LOCALES', function($LOCALES) {
+  module.config(['$LOCALES', function ($LOCALES) {
     $LOCALES.push('georchestra');
   }]);
 
@@ -18,86 +16,104 @@
    */
   module.run([
     'gnSearchSettings',
-    'georLinkActionsService',
+    'gnExternalViewer',
+    'gnGlobalSettings',
+    '$filter',
+    'gnLangs',
+    '$window',
+    'gnHttp',
+    '$location',
+    'gnAlertService',
+    function (gnSearchSettings, gnExternalViewer, gnGlobalSettings, $filter, gnLangs, $window, gnHttp, $location, gnAlertService) {
 
-    function(gnSearchSettings, georLinkActionsService) {
+      var settings = gnGlobalSettings.gnCfg.mods.map.externalViewer;
+      var baseMdUrl = $location.absUrl().split('#')[0] + '#/metadata/';
+
       gnSearchSettings.linkTypes = {
         links: ['LINK'],
         downloads: ['DOWNLOAD'],
-        layers:['OGC', 'vnd.ogc.wms_xml'],
+        layers: ['OGC:WMS'],
         maps: ['ows']
       };
 
       // Add custom actions in selection action button
       gnSearchSettings.customSelectActions = [{
         icon: 'fa-globe',
-        fn: function() {
-          georLinkActionsService.extractMetadata('mapfishapp');
+        fn: function(id) {
+          gnHttp.callService('selectionLayers', {
+            id: id && angular.isString(id) ? id : undefined
+          }).then(function(response) {
+            var wmcCount = 0;
+            var wmsCount = 0;
+
+            var processEntries = function (item) {
+              item.metadataURL = baseMdUrl + item.muuid;
+              switch (item.owstype) {
+                case 'WMC':
+                  wmcCount += 1;
+                  break;
+
+                case 'WMS':
+                  wmsCount += 1;
+                  break;
+              }
+            };
+            response.data.services.map(processEntries);
+            response.data.layers.map(processEntries);
+
+            if (wmsCount == 0) {
+              gnAlertService.addAlert({
+                msg: 'invalidSelectionZeroWMS',
+                type: 'danger'
+              });
+              return;
+            }
+
+            response.data.layers.filter(function (layer) {
+              return layer.owstype === 'WMS'
+            }).forEach(function (layer) {
+              var service = {
+                name: layer.layername,
+                url: layer.owsurl
+              };
+              var md = {
+                uui: layer.muuid
+              };
+              gnExternalViewer.viewService(md, service);
+            });
+            gnExternalViewer._commit();
+          });
         },
         label: 'viewLayers'
-      }, {
-        icon: 'fa-eject',
-        fn: function() {
-          georLinkActionsService.extractMetadata('mapfishapp/?addons=extractor_0');
-        },
-        label: 'extractLayers'
       }];
+
+      gnExternalViewer._commit = function () {
+        if (!this._toView.length) {
+          return;
+        }
+
+        var layers = this._toView.map(function (toView) {
+          var name = toView.service.name;
+          return encodeURIComponent($filter('gnLocalized')(name) || name || '');
+        }).map(function(name) { return '"' + name + '"' }) || '';
+
+        var services = this._toView.map(function (toView) {
+          var url = toView.service.url;
+          return JSON.stringify({
+            type: toView.service.type,
+            url: encodeURIComponent($filter('gnLocalized')(url) || url || '')
+          })
+        }).join(settings.valueSaparator || ',');
+
+        var url = settings.urlTemplate
+          .replace('${service.url}', services)
+          .replace('${service.name}', layers)
+
+        settings.openNewWindow ? $window.open(url, '_blank') :
+          $window.location = url;
+
+        // reset list of services to view
+        this._toView.length = 0;
+      }.bind(gnExternalViewer);
     }]);
-
-
-  /**
-   * ExtendMainController is a constructor of a new controller that is here
-   * to extend the `gnsDefault` controller, containing main logic of the
-   * default search page.
-   *
-   * @param {Object} $scope is shared with gnsDefault scope
-   * @param {Object} georLinkActionsService
-   * @constructor
-   */
-  var ExtendMainController = function($scope, georLinkActionsService, gnRelatedResources) {
-    if($scope.resultviewFns) {
-
-      // Overrides add wms to map buttons action in main search scope
-      $scope.resultviewFns.addMdLayerToMap = georLinkActionsService.addWMSLayer;
-      $scope.resultviewFns.getLayerJSONSpec = georLinkActionsService.getLayerJSONSpec;
-      $scope.resultviewFns.addWMSLayers = georLinkActionsService.addWMSLayers;
-      $scope.resultviewFns.addAllMdLayersToMap = function(layers, md) {
-        georLinkActionsService.extractMetadata('mapfishapp', md.getId());
-      }
-    }
-    gnRelatedResources.configure({
-      'WMS' : {
-        iconClass: 'fa-globe',
-        label: 'addToMap',
-        action: georLinkActionsService.addWMSLayer
-      },
-      'WFS' : {
-        iconClass: 'fa-globe',
-        label: 'addToMap',
-        action: null
-      },
-      'WMSSERVICE' : {
-        iconClass: 'fa-globe',
-        label: 'addServiceLayersToMap',
-        action: georLinkActionsService.addWMSLayer
-      }
-    });
-  };
-
-  module.directive('gnExtendMainctrl', [
-    function() {
-      return {
-        restrict: 'E',
-        scope: false,
-        controller: ExtendMainController
-      };
-    }
-  ]);
-
-  ExtendMainController['$inject'] = [
-    '$scope',
-    'georLinkActionsService',
-    'gnRelatedResources'
-  ];
-
-})();
+})()
