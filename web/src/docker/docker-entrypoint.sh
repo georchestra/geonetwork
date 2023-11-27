dcdd078eb2572f4efa63ffcfb1e4842b7d7ff834
Florian Necas <florian.necas@camptocamp.com>
2023-11-27 16:28:41 +0100

    fix : remove comment

--- web/src/docker/docker-entrypoint.sh
+++ web/src/docker/docker-entrypoint.sh
@@ -7,13 +7,13 @@ if [[ -z "${CUSTOM_SCRIPTS_DIRECTORY}" ]]; then
   echo "[INFO] No CUSTOM_SCRIPTS_DIRECTORY env variable set"
 else
   echo "[INFO] CUSTOM_SCRIPTS_DIRECTORY env variable set to ${CUSTOM_SCRIPTS_DIRECTORY}"
-  # Regex is needed in jetty9 images, but not alpine's ones.
   cp -v "${CUSTOM_SCRIPTS_DIRECTORY}"/* "$DIR"
   echo "[INFO] End copying custom scripts"
 fi
 
 if [[ -d "$DIR" ]]
 then
+  # Regex is needed to execute all kind of files, including sh files. Warning : --regex not available in alpine images.
   /bin/run-parts --verbose "$DIR" --regex='.*'
 fi
 
